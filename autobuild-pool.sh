937b1cfeddcb4e90d53e266e8600988566d90930
wouterwijsman <wouter.wijsman@osudio.com>
2016-01-07 13:11:57 +0100

    Updated multiple packages and create autobuild-pool.sh script The script allows for redownloading the entire pool.

---
+++ autobuild-pool.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# remove old pool
+if [ -d pool ]; then
+	rm -rf pool
+fi
+
+# redownload all packages
+for i in steamos debian backports; do
+	./downloadpackages.sh update autobuild-config/$i.repo
+	./downloadpackages.sh download `cat autobuild-config/$i.packages|tr "\n" "\ "`
+done
+
+# sync vaporos packages
+rsync -av vaporos-packages/ pool
+
+# done
